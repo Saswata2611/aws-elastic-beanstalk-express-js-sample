595559e69e5899ac3960170343d56995d3a5ad54
Saswata2611 <rishumukherjee2003@gmail.com>
2024-08-13 03:01:22 +0530

    commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/post', async(req, res)=>{
+    res.status(200).json("Hello Sir");
+})
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
